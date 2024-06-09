014e0c171298dd42f1edbdd40bfd4f0363599cbd
Sachin singh <singh.sachin@resourcifi.com>
2024-06-09 21:25:15 +0530

    [Refaactor]::unwanted dependecies removed from codebase

--- src/myComponents/Navbar/MobileDrawer.jsx
+++ src/myComponents/Navbar/MobileDrawer.jsx
@@ -1,90 +1,85 @@
 import Link from "next/link";
 import {
-    Sheet,
-    SheetClose,
-    SheetContent,
-    SheetTrigger,
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
 } from "../../../components/ui/sheet";
 import clsx from "clsx";
-import Image from 'next/image';
+import Image from "next/image";
 import { usePathname } from "next/navigation";
-// import Link from "next/link";
 
 function MobileDrawer({ isOpen, onClose }) {
-    const navLinks = [
-        {
-            name: "Home",
-            link: "/"
-        },
-        {
-            name: "Destinations",
-            link: "/Destination"
-        },
-        {
-            name: "Blog",
-            link: "/Blog"
-        },
-        {
-            name: "About",
-            link: "/About"
-        },
-        {
-            name: "ContactUs",
-            link: "/ContactUs"
-        }
-    ]
-    const pathname = usePathname();
-    return (
-        <Sheet>
-            <SheetTrigger asChild>
-                <div className="lg:hidden">
-                    <Image src={'/category.svg'} width={30} height={30} alt="menuBar" />
-                </div>
-                {/* <Button className="lg:hidden" variant="outline">Open</Button> */}
-            </SheetTrigger>
-            <SheetContent>
-                <div className="grid w-[200px] p-4">
+  const navLinks = [
+    {
+      name: "Home",
+      link: "/",
+    },
+    {
+      name: "Destinations",
+      link: "/Destination",
+    },
+    {
+      name: "Blog",
+      link: "/Blog",
+    },
+    {
+      name: "About",
+      link: "/About",
+    },
+    {
+      name: "ContactUs",
+      link: "/ContactUs",
+    },
+  ];
+  const pathname = usePathname();
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <div className="lg:hidden">
+          <Image src={"/category.svg"} width={30} height={30} alt="menuBar" />
+        </div>
+        {/* <Button className="lg:hidden" variant="outline">Open</Button> */}
+      </SheetTrigger>
+      <SheetContent>
+        <div className="grid w-[200px] p-4">
+          {navLinks.map((link, index) => {
+            return (
+              <div key={index}>
+                <SheetClose asChild>
+                  <Link
+                    className={clsx(
+                      "flex h - [48px] grow items - center justify - center gap - 2 rounded - md bg - gray - 50 p - 3 text - sm font - medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+                      { "bg-sky-100 text-blue-600": pathname === link.href }
+                    )}
+                    href={link.link}
+                  >
+                    {/* <a className="text-gray-500 hover:text-gray-900"> */}
+                    <p className=" text-sm font-semibold leading-6 text-gray-900text-gray-500 hover:text-gray-900">
+                      {link.name}
+                    </p>
+                    {/* </a> */}
+                  </Link>
+                </SheetClose>
+              </div>
+            );
+          })}
+        </div>
 
-
-                    {
-                        navLinks.map((link, index) => {
-                            return (
-                                <div key={index}>
-
-                                    <SheetClose asChild>
-                                        <Link className={clsx(
-                                            "flex h - [48px] grow items - center justify - center gap - 2 rounded - md bg - gray - 50 p - 3 text - sm font - medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
-                                            { "bg-sky-100 text-blue-600": pathname === link.href }
-                                        )} href={link.link} >
-                                            {/* <a className="text-gray-500 hover:text-gray-900"> */}
-                                            <p className=" text-sm font-semibold leading-6 text-gray-900text-gray-500 hover:text-gray-900">
-                                                {link.name}
-                                            </p>
-                                            {/* </a> */}
-                                        </Link>
-                                    </SheetClose>
-                                </div>
-                            )
-                        })
-                    }
-                </div>
-
-                {/* <SheetHeader>
+        {/* <SheetHeader>
                     <SheetTitle>Edit profile</SheetTitle>
                     <SheetDescription>
                         {`Make changes to your profile here. Click save when you're done.`}
                     </SheetDescription>
                 </SheetHeader> */}
 
-                {/* <SheetFooter>
+        {/* <SheetFooter>
                     <SheetClose asChild>
                         <Button type="submit">Save changes</Button>
                     </SheetClose>
                 </SheetFooter> */}
-
-            </SheetContent>
-        </Sheet >
-
-    );
+      </SheetContent>
+    </Sheet>
+  );
 }
-export default MobileDrawer
+export default MobileDrawer;
