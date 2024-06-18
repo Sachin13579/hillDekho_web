4faf4a74b6a105cf2a3a11d69ec832bfcf086712
Sachin singh <sachin.singh.glbimr19@gmail.com>
2024-06-18 10:49:56 +0530

    [Feature]::login singup pages added.

--- src/myComponents/Navbar/Userdropdown.jsx
+++ src/myComponents/Navbar/Userdropdown.jsx
@@ -1,10 +1,48 @@
+import clsx from "clsx";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 const UserMenu = () => {
+  const links = [
+    { name: "Signup", link: "/Signup" },
+    { name: "Login", link: "/Login" },
+    { name: "Profile", link: "/Profile" },
+    { name: "Logout", link: "/Logout" },
+  ];
+  const pathname = usePathname();
   return (
     <div
       id="dropdownAvatar"
       className="absolute right-0 z-50  bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
     >
       <div className="px-4 py-3 text-sm text-gray-900 dark:text-white">
+        <ul
+          className="py-2 text-sm text-gray-700 dark:text-gray-200"
+          aria-labelledby="dropdownUserAvatarButton"
+        >
+          {links.map((link) => {
+            return (
+              <Link
+                href={link.link}
+                key={link.name}
+                className={clsx(
+                  "flex h - [48px] grow items - center justify - center gap - 2 rounded - md bg - gray - 50 p - 3 text - sm font - medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+                  { "bg-sky-100 text-blue-600": pathname === link.href }
+                )}
+              >
+                <p>{link.name}</p>
+              </Link>
+            );
+          })}
+          {/* <li className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+            Signup
+          </li>
+          <li className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+            Login
+          </li> */}
+        </ul>
+      </div>
+      {/* <div className="px-4 py-3 text-sm text-gray-900 dark:text-white">
         <div>Bonnie Green</div>
         <div className="font-medium truncate">name@flowbite.com</div>
       </div>
@@ -44,7 +82,7 @@ const UserMenu = () => {
         >
           Sign out
         </a>
-      </div>
+      </div> */}
     </div>
   );
 };
